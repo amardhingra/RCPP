c45f53b0f2df688c9dd11a0e4da6f227219d9041
Julie <julie.y.chien@gmail.com>
2016-04-11 17:42:56 -0400

    working on hello world, this commit contains a lot of commented out code that may be useful alter

--- Tests/helloworld.cpp
+++ Tests/helloworld.cpp
@@ -1,12 +1,19 @@
 // must have boost installed
 // with homebrew: brew install boost
-// this does not work yet!
+
+// assumptions: streams are immutable, but can be mirrored
+// refs: Java SE7 Observable, RxJava, RxJS, boost::signals2, boost::thread, wiki page for observer pattern, 
+// started with diff sources and streams but saw no need for the extra layer of abstraction, at least for this example
 
 #include <iostream>
 #include <vector>
-//#include <functional>
 #include <thread>
 #include <boost/thread.hpp>
+#include <boost/bind.hpp>
+
+// thread-safe signal handling, used for the notification system
+//http://www.boost.org/doc/libs/1_43_0/doc/html/signals2.html
+#include <boost/signals2.hpp> 
 
 using namespace std;
 
@@ -16,56 +23,136 @@ using namespace std;
 class event {
 public:
 	string data;
+	event(string);
 };
+
+event::event(string new_data) {
+	data = new_data;
+}
 
 // A source is the input to a stream
 class source {
 public:
-	bool has_next = 0;
+	bool has_next = false;
 	event get_next;
 
 	void get_keyboard_input(void) {
 		while(true) {
+			/*
 			string keyinput;
 			cin >> keyinput;
-			cout << "u entered " << keyinput << endl;
+			cout << "u entered " << keyinput << endl;*/
+			cout << "rofl ";
 		}
 	}
 };
 
-// A stream accumulates sources (currenty for testing, a stream only has one source)
-class stream {
-public:
-	source my_source;
-
-};
-
-// A subscriber listens to a stream
+// A subscriber listens to one or more streams (for testing, a subscriber has one stream)
 // It has a handler for handling the stream's events
 class subscriber {
-	stream my_stream;
+	//stream my_stream;
 public:
-	std::function<void()> handler;
+	// Notifies the subscriber of a new element in the sequence
+	//std::function<void(string)> on_next;
+		std::function<void(event new_event)> on_next;
 };
 
+// A stream accumulates sources (currently for testing, a stream only has one source)
+class stream {
+	// indicates if there's a new value ready for subscribers to handle
+	bool changed = false;
+	subscriber my_subscriber;
 
-// Don't try to make too much sense of this main, just testing out stuff
+
+public:
+	//source my_source;
+
+	void setChanged() {
+		changed = true;
+	}
+
+	void subscribe(subscriber new_subscriber) {
+		my_subscriber = new_subscriber;
+	}
+
+	void notifySubscribers(event new_event) {
+		my_subscriber.on_next(new_event);
+		changed = false;
+	}
+
+	void get_keyboard_input() {
+		while(true) {
+			
+			string keyinput;
+			cin >> keyinput;
+			
+			setChanged();
+			notifySubscribers(keyinput);
+			//cout << "lmao ";
+		}
+	}
+
+};
+
+void printlol() {
+	while (true) {
+		cout << "lol ";
+	}
+}
+
+struct Hello {
+	void operator()() const {
+		cout << "hello  ";
+	}
+};
+
+struct World {
+	void operator()() const {
+		cout << "world ";
+	}
+};
+
 int main() {
-	source keyboard_source;
+	//source keyboard_source;
 	stream keyboard_stream;
-	keyboard_stream.my_source = keyboard_source;
+	//keyboard_stream.my_source = keyboard_source;
 
 	subscriber mysub;
+	mysub.on_next = [](event new_event) { cout << "lol" << endl; };
 
-	mysub.handler = []() { cout << "hello" << endl; };
+	keyboard_stream.subscribe(mysub);
 
-	thread t1(keyboard_source.get_keyboard_input);
+
+
+	boost::thread t2(boost::bind(&stream::get_keyboard_input, &keyboard_stream));
+	t2.join();
+
+		//mysub.on_next = []() { cout << "lol " << endl; };
+/*
+	boost::signals2::signal<void ()> sig;
+	sig.connect(mysub.on_next);
+	sig.connect(World());
+	sig();
+	sig();
+	sig();*/
+
+/*
+	boost::thread t1{printlol};
+	//t.join();
+	boost::thread t2(boost::bind(&source::get_keyboard_input, &keyboard_source));
+	t2.join();
 	t1.join();
+*/
+	//thread t1(keyboard_source.get_keyboard_input);
 
-	mysub.handler();
+	//t1.join();
+
+//	mysub.on_next("poo");
+		mysub.on_next(event("poo"));
+/*
 	string keyinput;
 	cin >> keyinput;
-	cout << "u entered " << keyinput << endl;
+	cout << "u entered " << keyinput << endl;*/
 	return 0;
 }
 
