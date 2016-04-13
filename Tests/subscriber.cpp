ec3e68406f87f9191994ce2e2fbeb1a7be49273c
Amar Dhingra <amardhingra@gmail.com>
2016-04-13 03:12:01 -0400

    documented code

--- Tests/subscriber.cpp
+++ Tests/subscriber.cpp
@@ -3,8 +3,15 @@
 #include <iostream>
 #include <chrono>
 
+
+/*
+ * copy constructor for subscriber_event
+ */
 subscriber_event::subscriber_event(const subscriber_event& event){
+    // copy type
     type = event.type;
+
+    // copy correct variable from data
     switch(type){
         case message_type::BOOL:
             data.b = event.data.b;
@@ -21,60 +28,89 @@ subscriber_event::subscriber_event(const subscriber_event& event){
     }
 }
 
-void
-subscriber_pool::handle_event(){
+
+// worker thread function
+void subscriber_pool::handle_event(){
     while(true){
+        // acquire lock
         std::unique_lock<std::mutex> lk(lock);
+        
+        // wait until told to exit or while queue is empty
         while(!end && events.size() == 0){
             cond.wait(lk);
+
+            // exit on flag
             if(end){
                 std::cout << "thread exiting" << std::endl;
                 lk.unlock();
                 return;
             }
         }
+
+        // get event from queue and remove it from queue
         struct event ev = events.front();
         events.pop_front();
         lk.unlock();
 
+        // call the on_next function for the corresponding subscriber
         subscribers.at(ev.s_id).on_next(ev.s_event);
     }
 }
 
-void
-subscriber_pool::notify(sub_id id, subscriber_event event){
+
+void subscriber_pool::notify(sub_id id, subscriber_event event){
+    // create an event to add to the queue
     struct event ev(id, event);
+
+    // lock the queue and add the event to it
     std::unique_lock<std::mutex> lk(lock);
     events.push_back(ev);
     lk.unlock();
+
+    // wake up any worker threads
     cond.notify_all();
 }
 
-sub_id
-subscriber_pool::register_subscriber(subscriber &sub)
-{
+
+sub_id subscriber_pool::register_subscriber(subscriber &sub) {
+    // add the subscriber to the map
     subscribers.insert(std::pair<sub_id, subscriber&>(id, sub));
+    
+    // increment and return the counter
     return id++;
 }
 
+
 subscriber_pool::subscriber_pool(int concurrency){
-    for(int i = 0; i < concurrency; i++){
+    // never make more than 16 threads
+    int max = concurrency > 16 ? 16 : concurrency;
+
+    // create threads based on the concurrency requested
+    for(int i = 0; i < max; i++){
         pool.push_back(std::thread(&subscriber_pool::handle_event, this));
-        //std::thread t(&subscriber_pool::handle_event, this);
-        //t.detach();
     }
 }
 
+
 subscriber_pool::~subscriber_pool(){
+    // set the end flag
     std::unique_lock<std::mutex> lk(lock);
     end = true;
-    cond.notify_all();
     lk.unlock();
+
+    // wake up all worker threads
+    cond.notify_all();
+
+    // join all worker threads
     for(auto& t : pool){
         t.join();
     }
 }
 
+
+/*
+ * TESTING
+ */
 void func1(subscriber_event event){
     using namespace std;
     std::this_thread::sleep_for (std::chrono::seconds(1));
