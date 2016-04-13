56241deef2bd12617437fb1bfff05555de1fe504
Amar Dhingra <amardhingra@gmail.com>
2016-04-13 01:32:16 -0400

    Working implementation of subscriber pool, has weird memory issues on exit

--- Tests/subscriber.cpp
+++ Tests/subscriber.cpp
@@ -1,1 +1,116 @@
 #include "subscriber.h"
+
+#include <iostream>
+#include <chrono>
+
+subscriber_event::subscriber_event(const subscriber_event& event){
+    type = event.type;
+    switch(type){
+        case message_type::BOOL:
+            data.b = event.data.b;
+            break;
+        case message_type::INT:
+            data.i = event.data.i;
+            break;
+        case message_type::FLOAT:
+            data.f = event.data.f;
+            break;
+        case message_type::STR:
+            data.s = event.data.s;
+            break;
+    }
+}
+
+void
+subscriber_pool::handle_event(){
+    while(true){
+        std::unique_lock<std::mutex> lk(lock);
+        while(!end && events.size() == 0){
+            cond.wait(lk);
+            if(end){
+                std::cout << "thread exiting" << std::endl;
+                lk.unlock();
+                return;
+            }
+        }
+        struct event ev = events.front();
+        events.pop_front();
+        lk.unlock();
+
+        subscribers.at(ev.s_id).on_next(ev.s_event);
+    }
+}
+
+void
+subscriber_pool::notify(sub_id id, subscriber_event event){
+    struct event ev(id, event);
+    std::unique_lock<std::mutex> lk(lock);
+    events.push_back(ev);
+    lk.unlock();
+    cond.notify_all();
+}
+
+sub_id
+subscriber_pool::register_subscriber(subscriber &sub)
+{
+    subscribers.insert(std::pair<sub_id, subscriber&>(id, sub));
+    return id++;
+}
+
+subscriber_pool::subscriber_pool(int concurrency){
+    for(int i = 0; i < concurrency; i++){
+        pool.push_back(std::thread(&subscriber_pool::handle_event, this));
+        //std::thread t(&subscriber_pool::handle_event, this);
+        //t.detach();
+    }
+}
+
+subscriber_pool::~subscriber_pool(){
+    std::unique_lock<std::mutex> lk(lock);
+    end = true;
+    cond.notify_all();
+    lk.unlock();
+    for(auto& t : pool){
+        t.join();
+    }
+}
+
+void func1(subscriber_event event){
+    using namespace std;
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+    cout << "func 1" << endl;
+}
+
+void func2(subscriber_event event){
+    using namespace std;
+    cout << "func 2" << endl;
+}
+
+void func3(subscriber_event event){
+    using namespace std;
+    cout << "func 3" << endl;
+}
+
+void func4(subscriber_event event){
+    using namespace std;
+    cout << "func 4" << endl;
+}
+
+int main(void){
+    using namespace std;
+    subscriber_pool pool(4);
+    subscriber s1(func1);
+    subscriber s2(func2);
+    subscriber s3(func3);
+    subscriber s4(func4);
+    pool.register_subscriber(s1);
+    pool.register_subscriber(s2);
+    pool.register_subscriber(s3);
+    pool.register_subscriber(s4);
+    pool.notify(0,1);
+    pool.notify(0,1);
+    pool.notify(0,1);
+    pool.notify(0,1);
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return 0;
+}
--- Tests/subscriber.h
+++ Tests/subscriber.h
@@ -1,85 +1,122 @@
 #ifndef INCLUDED_SUBSCRIBER
 #define INCLUDED_SUBSCRIBER
 
+
+#include <condition_variable>
 #include <functional>
+#include <list>
+#include <map>
+#include <mutex>
 #include <string>
+#include <vector>
+#include <thread>
+#include <iostream>
+
+typedef unsigned long sub_id;
+
+class subscriber_event {
+// structures for storing the event
+public:
+    enum message_type {
+        BOOL  = 0,
+        INT   = 1,
+        FLOAT = 2,
+        STR   = 3
+    };
+private:
+    union message_data {
+        bool        b;
+        int         i;
+        float       f;
+        std::string s;
+    public:
+        message_data() {};
+        message_data(bool _b) : b(_b) {};
+        message_data(int _i) : i(_i) {};
+        message_data(float _f) : f(_f) {};
+        message_data(std::string _s) : s(_s) {};
+        ~message_data(){};
+    };
+
+// data in the message
+private:
+    message_type type;
+    message_data data;
+
+// contructors and data accessor methods
+public:
+    subscriber_event(bool _b) : type(message_type::BOOL), data(_b) {};
+    subscriber_event(int _i) : type(message_type::INT), data(_i) {};
+    subscriber_event(float _f) : type(message_type::FLOAT), data(_f) {};
+    subscriber_event(std::string _s) : type(message_type::STR), data(_s) {};
+    subscriber_event(const subscriber_event &event);
+public:
+    message_type get_type() { return type; }
+    bool         get_b() { return data.b; }
+    int          get_i() { return data.i; }
+    float        get_f() { return data.f; }
+    std::string  get_s() { return data.s; }
+};
+
+
+class subscriber {
+public:
+    std::function<void(subscriber_event)> on_next;
+    std::function<void(std::exception)> on_error;
+    std::function<void()> on_completed;
+
+
+public:
+    static void simple_on_next(subscriber_event n){
+        // TODO: define behavior for simple next handler
+    }
+
+    static void simple_on_error(std::exception e){
+        // TODO: define behavior for simple error handler
+    }
+
+    static void simple_on_completed(){
+        // TODO: define behavior for simple completed handler
+    }
+
+public:
+    subscriber(
+        std::function<void(subscriber_event)> next,
+        std::function<void(std::exception)> error = simple_on_error,
+        std::function<void()> completed = simple_on_completed) :
+    on_next(next), on_error(error), on_completed(completed) {};
+
+    subscriber(subscriber &sub) :
+        on_next(sub.on_next), on_error(sub.on_error), on_completed(sub.on_completed) {};
+
+};
 
 
 class subscriber_pool {
 
-public:
-    class subscriber_event {
-    // structures for storing the event
+private:
+    class event {
     public:
-        enum message_type {
-            BOOL  = 0,
-            INT   = 1,
-            FLOAT = 2,
-            STR   = 3
-        };
-    private:
-        union message_data {
-            bool        b;
-            int         i;
-            float       f;
-            std::string s;
-        public:
-            message_data(bool _b) : b(_b) {};
-            message_data(int _i) : i(_i) {};
-            message_data(float _f) : f(_f) {};
-            message_data(std::string _s) : s(_s) {};
-            ~message_data(){};
-        };
-
-    // data in the message
-    private:
-        message_type type;
-        message_data data;
-
-    // contructors and data accessor methods
-    public:
-        subscriber_event(bool _b) : type(message_type::BOOL), data(_b) {};
-        subscriber_event(int _i) : type(message_type::INT), data(_i) {};
-        subscriber_event(float _f) : type(message_type::FLOAT), data(_f) {};
-        subscriber_event(std::string _s) : type(message_type::STR), data(_s) {};
-
-    public:
-        message_type get_type() { return type; }
-        bool         get_b() { return data.b; }
-        int          get_i() { return data.i; }
-        float        get_f() { return data.f; }
-        std::string  get_s() { return data.s; }
+        sub_id           s_id;
+        subscriber_event s_event;
+        event(sub_id _id, subscriber_event _event) : s_id(_id), s_event(_event) {};
     };
+    bool                                   end = false;
+    sub_id                                 id = 0;
+    std::map<sub_id, subscriber&>          subscribers;
+    std::list<event>                       events;
+    std::mutex                             lock;
+    std::condition_variable                cond;
+    std::vector<std::thread>               pool;
+
+    void handle_event();
 
 public:
-    class subscriber {
-    public:
-        std::function<void(subscriber_pool::subscriber_event)> on_next;
-        std::function<void(std::exception)> on_error;
-        std::function<void()> on_completed;
+    subscriber_pool(int concurrency = 1);
+    ~subscriber_pool();
+    void notify(sub_id id, subscriber_event);
+    sub_id register_subscriber(subscriber& sub);
 
-
-    public:
-        static void simple_on_next(subscriber_pool::subscriber_event n){
-            // TODO: define behavior for simple next handler
-        }
-
-        static void simple_on_error(std::exception e){
-            // TODO: define behavior for simple error handler
-        }
-
-        static void simple_on_completed(){
-            // TODO: define behavior for simple completed handler
-        }
-
-    public:
-        subscriber(
-            std::function<void(subscriber_pool::subscriber_event)> next,
-            std::function<void(std::exception)> error = simple_on_error,
-            std::function<void()> completed = simple_on_completed) :
-        on_next(next), on_error(error), on_completed(completed) {};
-
-    };
 };
 
 #endif
