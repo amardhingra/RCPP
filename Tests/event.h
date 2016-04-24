70b1990f775afe63af47114aaecab6b29de570f0
Iris <iz2140@columbia.edu>
2016-04-24 14:59:24 -0400

    Adding event header file

---
+++ Tests/event.h
@@ -0,0 +1,24 @@
+template <class T>
+
+class event {
+
+private:
+    T _data;
+
+
+public:
+    // constructors for supported types
+    // set the correct member of the enum and assign the correct selector
+    event(T data): _data (data) {};
+
+    event(const event &event): _data (event.data) {};
+
+public:
+    // accessors
+   
+    const T& get_data() { return _data; }
+    // int          get_i() { return data.i; }
+    // float        get_f() { return data.f; }
+    // std::string  get_s() { return data.s; }
+
+}; // end subscriber_event
