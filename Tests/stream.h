856fc2b5827b26bb22d441a0f98beee1f704ed47
Amar Dhingra <amardhingra@gmail.com>
2016-04-24 19:30:19 -0400

    Added default type for stream output template argument

--- Tests/stream.h
+++ Tests/stream.h
@@ -13,7 +13,7 @@ typedef unsigned long stream_id;
 //namespace {class subscriber_pool; }
 //namespace {class subscriber; }
 
-template <typename T>
+template <typename InputType, typename OutputType = InputType>
 class stream {
     //initially set to 0
 	static stream_id id; 
@@ -22,7 +22,7 @@ class stream {
 
     // The threadpool that is assigned to this stream. If none are specified then it gets initialized??
     //seems like there would be ownership problems with this model
-    subscriber_pool<T> *thread_pool; 
+    subscriber_pool<InputType> *thread_pool; 
 
     public:
     void change(){
@@ -41,13 +41,13 @@ class stream {
     public:
     //default constructor, will eventually take from a source. 
     stream(){
-        subscriber_pool<T> pool(2);
+        subscriber_pool<InputType> pool(2);
         thread_pool = &pool;
         id++;
     }; 
 
     //you can specify a specific subscriber_pool
-    stream(subscriber_pool<T>* some_pool) {
+    stream(subscriber_pool<InputType>* some_pool) {
         thread_pool = some_pool; 
         id++;
     };  
@@ -61,17 +61,17 @@ class stream {
     std::function<void()> get_events_from_source;
 
     // Add a new subscriber to this stream
-    void register_subscriber(subscriber<T> new_subscriber){
+    void register_subscriber(subscriber<InputType> new_subscriber){
         thread_pool->register_subscriber(new_subscriber, id);
     };
-    void register_subscriber(std::vector<subscriber<T>> new_subscribers){
-        for (subscriber<T> new_subscriber : new_subscribers )
+    void register_subscriber(std::vector<subscriber<InputType>> new_subscribers){
+        for (subscriber<InputType> new_subscriber : new_subscribers )
             thread_pool->register_subscriber(new_subscriber, id);
     };
 
     // If the stream has changed, then notify all subscribers and clear the "changed" variable to indicate that the stream has no longer changed
     //template<class T> 
-    void notify_subscribers(event<T> new_event){
+    void notify_subscribers(event<InputType> new_event){
          if (this->has_changed()) {
         // auto pool_subscribers = thread_pool->pool;
         // for (subscriber pool_subscriber : pool_subscribers)
@@ -93,7 +93,7 @@ class stream {
 
 };
 
-template <typename T>
-stream_id stream<T>::id;
+template <typename InputType, typename OutputType>
+stream_id stream<InputType, OutputType>::id;
 
 #endif
