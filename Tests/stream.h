2ffea21aaad5005449098ed3cefbf48e7fd7964d
Amar Dhingra <amardhingra@gmail.com>
2016-04-27 00:07:09 -0400

    Reformatted stream.h and reimplemented api

--- Tests/stream.h
+++ Tests/stream.h
@@ -5,247 +5,249 @@
 #include <boost/thread.hpp>
 #include <thread>
 #include <functional>
+#include <memory>
 
 #include "subscriber.h"
 #include "event.h"
 
 typedef unsigned long stream_id;
+
+
 /*
  * A stream emits data for subscribers to the stream to handle.
  * A stream can have many subscribers.
  */
-
-
 template <typename InputType, typename OutputType = InputType>
 class stream {
 
+/* --------------------- STREAM MEMBER VARIABLES -------------------*/
 private:
 
-    static stream_id            unique_id;   // unique id generator for streams
-    stream_id                   id;          // specific id for instance of stream
-    subscriber_pool<InputType> *thread_pool; // subscriber pool to use for handling events
-    bool                        owner;       // determines if the current object is the owner of the pool
-    std::thread                 t;           // thread on which to run the on_start function
+    static stream_id                             unique_id;   // unique id generator for streams
+    stream_id                                    id;          // specific id for instance of stream
+    std::shared_ptr<subscriber_pool<InputType>>  thread_pool; // subscriber pool to use for handling events
+    std::thread                                  t;           // thread on which to run the on_start function
+    std::function<void(stream<InputType> & my_stream)> on_start; // function to be called to generate data
     //bool changed = false;
 
+/* ---------------- STREAM CONSTRUCTORS AND OPERATORS --------------*/
 
 public:
-    // default constructor
-    stream() : 
-        id(unique_id++),
-        thread_pool(new subscriber_pool<InputType>),
-        owner(true)
-        {}; 
 
     // constructor with shared thread pool
-    stream(subscriber_pool<InputType>* some_pool) :
+    stream(std::shared_ptr<subscriber_pool<InputType>> some_pool = std::shared_ptr<subscriber_pool<InputType>>(new subscriber_pool<InputType>),
+        const std::function<void(stream<InputType> & my_stream)> & on_start = nullptr) :
         id(unique_id++),
         thread_pool(some_pool),
-        owner(false)
+        on_start(on_start)
         {};
 
+    // 
     ~stream(){
         // wait for the stream to finish
         complete();
+    };
 
-        // check if we are the owner of the thread and delete it
-        if(owner && thread_pool != nullptr){
-             delete thread_pool;
-             thread_pool = nullptr;
+    // disable copy constructor
+    stream(const stream& stream_to_copy) = delete;
+
+    // assignment operator just assigns the reference
+    stream& operator=(stream &my_stream){
+        
+        #ifdef
+        std::cout << "stream: operator= called" << std::endl;
+        #endif
+        
+        return my_stream;
+    };
+
+    // move constructor
+    stream(stream &&other) : 
+        id(other.id),
+        thread_pool(other.thread_pool){
+        
+        #ifdef
+        std::cout << "stream: move constructor called" << std::endl;
+        #endif
+        
+        other.thread_pool = nullptr;
+    };
+
+    // move assignment operator
+    stream& operator=(stream&& other){
+        if (this != &other) {
+            // Free the existing resource.
+            delete thread_pool;
+
+            // Copy the thread_pool pointer and its id from the source object.
+            thread_pool = other.thread_pool;
+            id = other.id;
+
+             // Release the thread_pool pointer from the source object so that
+            // the destructor does not free the memory multiple times.
+            other.thread_pool = nullptr;
         }
-    }; 
+        return *this;
+    }
+
+/* --------------------- SUBSCRIBER FUNCTIONS -------------------*/
+public:        
 
 
     // Add a new subscriber to this stream
     void register_subscriber(subscriber<InputType> new_subscriber){
         thread_pool->register_subscriber(new_subscriber, id);
     };
-    void register_subscriber(std::vector<subscriber<InputType>> new_subscribers){
 
-        for (subscriber<InputType> new_subscriber : new_subscribers )
+    // Add a list of subscribers to this stream
+    void register_subscribers(std::vector<subscriber<InputType>> new_subscribers){
+        for (subscriber<InputType> new_subscriber : new_subscribers){
             thread_pool->register_subscriber(new_subscriber, id);
+        }
     };
 
-    // If the stream has changed, then notify all subscribers
-    //template<class T> 
-        void notify_subscribers(event<InputType> new_event){
-            thread_pool->notify_stream(id, new_event);
-        };
+
+    /*
+     * Function that is used to notify the stream of new incoming events
+     * the stream will notify the appropriate subscribers and invoke the 
+     * provided filter, map and reduce operations
+     */
+    void notify(event<InputType> new_event){
+        thread_pool->notify_stream(id, new_event);
+    };
 
         /* --------------JULIE'S MESS BELOW --------------- */
+/* ---------------- FUNCTIONS TO START AND STOP STREAM --------------*/
 
 public:
     // Starts the stream;
-        void start(){
+    void start(){
+        if(on_start != nullptr){
             t = std::thread(std::bind(on_start, std::ref(*this)));
-            //on_start(*this);
-        };
-
-        void complete(){
-            t.join();
         }
+    };
 
-    // create stream with pool and on_start
-    // TODO: create version of this w/o pool arg once stream's default constructor gets fixed
+    void complete(){
+        t.join();
+    }
 
-        //you can specify a specific subscriber_pool
-        stream(subscriber_pool<InputType>* some_pool, const std::function<void(stream<InputType> & my_stream)> & on_start) {
-            owner = false;
-            thread_pool = some_pool; 
-            id++;
-            this->on_start = on_start;
-        };
+/* -------------------------- FILTER/MAP/REDUCE ------------------------*/
 
-        // disable copy constructor
-        stream(const stream& stream_to_copy) = delete;
+public:
 
-        // assignment operator just assigns the reference
-        stream& operator=(stream &my_stream){
-            std::cout << "stream: operator= called" << std::endl;
-            return my_stream;
-        };
+    stream<InputType> filter(std::function<event<InputType>(event<InputType>) filter_func){
+        // TODO
+    }
 
-        // move constructor
-        stream(stream &&other) 
-            : id(other.id)
-            , thread_pool(other.thread_pool)
-             
-        {
-            std::cout << "stream: move constructor called" << std::endl;
-            //thread_pool = other.thread_pool;
-            //id = other.id;
-            other.thread_pool = nullptr;
-            //other.id = 0;
-        };
+    stream<InputType> map(std::function<event<OutputType>(event<InputType>)> new_map_func){
+        stream<InputType> new_stream;
+        new_stream.map_func = new_map_func;
+        this->children.push_back(&new_stream);
+        return new_stream;
 
-        // move assignment
-        stream& operator=(stream&& other){
-            if (this != &other)
-            {
-                // Free the existing resource.
-                delete thread_pool;
+    };
 
-                // Copy the thread_pool pointer and its id from the source object.
-                thread_pool = other.thread_pool;
-                id = other.id;
-
-                 // Release the thread_pool pointer from the source object so that
-                // the destructor does not free the memory multiple times.
-                other.thread_pool = nullptr;
-            }
-            return *this;
-        }
-
-        std::function<void(stream<InputType> & my_stream)> on_start;
-
-        // map 
-        // TODO: should it return by reference instead of value?
-        stream<InputType> map(std::function<event<InputType>(event<InputType>)> new_map_func) {
-            stream<InputType> new_stream;
-            new_stream.map_func = new_map_func;
-            this->children.push_back(&new_stream);
-            return new_stream;
-
-        };
-
+    stream<InputType> reduce(std::function<event<InputType>(event<InputType>) filter_func){
+        // TODO
+    }
 
 
 // functions/members prefixed with "st" are part of a single-threaded system that bypasses thread pool. Use these functions for debugging.
+/* -------------------------- SINGLE THREADED FUNCTIONS ------------------------*/
 
-        std::vector<subscriber<InputType>> st_my_subscribers;
+public:
+    std::vector<subscriber<InputType>> st_my_subscribers;
 
-        void st_register_subscriber(const subscriber<InputType> & new_subscriber){
-#ifdef DEBUG
-            std::cout << "registering to stream " << this << ". # of subscribers = " << st_my_subscribers.size() << "\n";
-#endif
-            st_my_subscribers.push_back(new_subscriber);
+    void st_register_subscriber(const subscriber<InputType> & new_subscriber){
+        #ifdef DEBUG
+        std::cout << "registering to stream " << this << ". # of subscribers = " << st_my_subscribers.size() << "\n";
+        #endif
+        st_my_subscribers.push_back(new_subscriber);
 
-#ifdef DEBUG
-            std::cout << "finished registering to stream " << this << ". # of subscribers = " << st_my_subscribers.size() << "\n";
-
-#endif
-        };
-
-
-        void st_notify_subscribers(event<InputType> new_event) {
-#ifdef DEBUG
-
-            std::cout << "stream " << this <<  " notifying " <<st_my_subscribers.size() << " subscribers" << std::endl;
-#endif
+        #ifdef DEBUG
+        std::cout << "finished registering to stream " << this << ". # of subscribers = " << st_my_subscribers.size() << "\n";
+        #endif
+    };
 
 
-            for (subscriber<InputType> my_subscriber : st_my_subscribers) {
-#ifdef DEBUG
+    void st_notify_subscribers(event<InputType> new_event) {
+        #ifdef DEBUG
+        std::cout << "stream " << this <<  " notifying " <<st_my_subscribers.size() << " subscribers" << std::endl;
+        #endif
 
-                std::cout << "stream " << this <<  " trying to notify subscriber " << my_subscriber.id << std::endl;
-#endif
+        for (subscriber<InputType> my_subscriber : st_my_subscribers) {
+            #ifdef DEBUG
+            std::cout << "stream " << this <<  " trying to notify subscriber " << my_subscriber.id << std::endl;
+            #endif
 
-                my_subscriber.on_next(new_event);
-
-            }
+            my_subscriber.on_next(new_event);
 
         }
 
-        void print_subscribers() {
-            std::cout << "subscribers: ";
-            for (auto my_subscriber : st_my_subscribers) {
-                std::cout << my_subscriber.id << " ";
-            }
-            std::cout << std::endl;
+    }
+
+    void print_subscribers() {
+        std::cout << "subscribers: ";
+        for (auto my_subscriber : st_my_subscribers) {
+            std::cout << my_subscriber.id << " ";
         }
+        std::cout << std::endl;
+    }
 
-        std::vector<stream<InputType> *> children;
+    std::vector<stream<OutputType> *> children;
 
-        std::function<event<InputType>(event<InputType>)> map_func;
+    std::function<event<OutputType>(event<InputType>)> map_func;
 
 
-        void notify_children(event<InputType> e) {
-            for (stream<InputType> *child_stream : children) {
+    void notify_children(event<OutputType> e) {
+        for (stream<OutputType> *child_stream : children) {
 
-                child_stream->on_receive_event_from_parent(e);
-            }
+            child_stream->on_receive_event_from_parent(e);
         }
+    }
 
-        void st_notify_children(event<InputType> e) {
-            for (stream<InputType> *child_stream : children) {
-#ifdef DEBUG
-
-                std::cout << "notifying child stream " <<  child_stream << std::endl;
-#endif
-                child_stream->st_on_receive_event_from_parent(e);
-            }
+    void st_notify_children(event<OutputType> e) {
+        for (stream<OutputType> *child_stream : children) {
+            #ifdef DEBUG
+            std::cout << "notifying child stream " <<  child_stream << std::endl;
+            #endif
+            child_stream->st_on_receive_event_from_parent(e);
         }
+    }
 
-        void on_receive_event_from_parent(event<InputType> e) {
-#ifdef DEBUG
-            std::cout << "stream " << this << " received event" << std::endl;
-#endif
-            if (map_func != NULL) {
-                e = map_func(e);
-            }
-#ifdef DEBUG
-            usleep(100);
-#endif
-            notify_subscribers(e);
-        };
+    void on_receive_event_from_parent(event<InputType> e) {
+        #ifdef DEBUG
+        std::cout << "stream " << this << " received event" << std::endl;
+        #endif
+        
+        if (map_func != NULL) {
+            e = map_func(e);
+        }
+        
+        #ifdef DEBUG
+        usleep(100);
+        #endif
+        
+        notify_subscribers(e);
+    };
 
-        void st_on_receive_event_from_parent(event<InputType> e) {
-#ifdef DEBUG
-            std::cout << "stream " << this << " received event" << std::endl;
-#endif
+    void st_on_receive_event_from_parent(event<InputType> e) {
+        #ifdef DEBUG
+        std::cout << "stream " << this << " received event" << std::endl;
+        #endif
 
-            if (map_func != NULL) {
-                event<InputType> new_e = map_func(e);
-                st_notify_subscribers(new_e);
-            }
-            else {
-                st_notify_subscribers(e);
+        if (map_func != NULL) {
+            event<InputType> new_e = map_func(e);
+            st_notify_subscribers(new_e);
+        }
+        else {
+            st_notify_subscribers(e);
 
-            }
-
-        };
+        }
 
     };
+
+};
 
 template <typename InputType, typename OutputType>
     stream_id stream<InputType, OutputType>::unique_id;
