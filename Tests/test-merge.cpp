054e1eebe76c54c28c0631b230f63d091fe906c5
Amar Dhingra <amardhingra@gmail.com>
2016-04-28 00:17:07 -0400

    Testing merge completed

--- Tests/test-merge.cpp
+++ Tests/test-merge.cpp
@@ -32,11 +32,36 @@ int main(void){
 
     stream<int> str3 = str1 + str2;
     str3.register_subscriber(subscriber<int>([](event<int> e){
-        std::cout << e.get_data() << std::endl;
+        std::cout << e.get_data() << ' ';
     }));
 
     str1.start();
     str2.start();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout << std::endl;
+
+    stream<int> str4(pool_str, [](stream<int>& str){
+        for(int i = 0; i < 100; i += 2){
+            str.notify(event<int>(i));
+        }
+    });
+
+    stream<int> str5(pool_str, [](stream<int>& str){
+        for(int i = 1; i < 100; i += 2){
+            str.notify(event<int>(i));
+        }
+    });
+
+    str4 += str5;
+
+    str4.register_subscriber(subscriber<int>([](event<int> e){
+        std::cout << e.get_data() << ' ';
+    }));
+
+    str4.start();
+    str5.start();
+    
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::cout << std::endl;
 }
